1a313add14fb0d6a936d949bd25a86b63a7f9141
pingze <719695274@qq.com>
2017-01-26 16:16:57 +0800

    添加使用说明

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # HuabanBatchUpload
 花瓣网图片批量上传工具
 
+## 使用
+0. 确保安装了requests库。
+1. 编辑index.py。
+2. 在浏览器中登录花瓣网后，找到Cookies填入index.py
+3. 将图片所在文件夹路径填入index.py
+4. 将指定画板名填入index.py
+5. 运行index.py
+
 ## 已实现
 + 上传文件（Python）
 + 添加文件到指定画板
@@ -10,3 +18,4 @@
 ## 待实现
 + 模拟登陆
 + 并发控制
++ GUI
--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from upload import upload,setCookies
 # ****** User Input ******
 # You must write your cookies below first
 
-cookies_str = "" #cookies
+cookies_str = "BDTUJIAID=87ad045ce860ea032fe2ed4f6c07918e; _hmt=1; _f=iVBORw0KGgoAAAANSUhEUgAAADIAAAAUCAYAAADPym6aAAABJElEQVRYR%2B1VOxYCIQwMF7KzsvFGXmW9kY2VnQfxCvgCRmfzCD9lnz53myWQAJOZBEfeeyIi7xz%2FyEXzZRPFhYbPc3hHXO6I6TbFixmfEyByeQQSxu6BcAXSkIGMazMjuBcz8pQcq44o0Iuyyc1p38C62kNsOdeSZDOQlLRQ80uOMalDgWCGMfsW2B5%2FATMUyGh2uhgptV9Ly6l5nNOa1%2F6zmjTqkH2aGEk2jY72%2B5k%2BNd9lBfLMh8GIP11iK95vw8uv7RQr4oNxOfbQ%2F7g5Z4meveyt0uKDEIiMLRC4jrG1%2FjkwKxCRE2e5lF30leyXYvQ628MZKV3q64HUFvnPAMkVuSWlEouLSiuV6dp2WtPBrPZ7uO5I18tbXWvEC27t%2BTcv%2Bx0JuJAoUm2L%2FQAAAABJRU5ErkJggg%3D%3D%2CWin32.1366.768.24; _tp-257aedc912c3569e1ea1e387539a138d8c2333f56a9f=1; referer=https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3D9sTbjXCRqNe9PEamSJT5k0l5eO-XyAuqMZUe7_MQqQbOVsh6WczJKIOoqH4p3u6d%26wd%3D%26eqid%3Dfa85c76b000823620000000358874b07; _ga=GA1.2.257306446.1478949278; __asc=2bf46e9b159d071fcf9e3df4c33; __auc=87f1b88c158583f12a3190a62f8; crtg_rta=crtnative3criteo_200x200_Pins%3Bcriteo_200x200_Search%3B; _cnzz_CV1256903590=is-logon%7Clogged-in%7C1485261673896%26urlname%7Cqjo3w1jgyk%7C1485261673896; CNZZDATA1256903590=1211073450-1478946017-%7C1485258621; uid=12697989; sid=Aj2RCNmAUuqFvrwmNrX2q7T5jnS.nST8HvQ2cIYi98j%2F3%2FOs%2BhtMfmuhk6jxAi2FIM2bl4M" #cookies
 
 path = "./huaban/test" # dirpath
 board_name = "测试" # board name
